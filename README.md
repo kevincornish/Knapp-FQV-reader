724be85c227af424b2448303eb4cddf4834f7fef
Kevin Cornish <kevinz_c@live.com>
2023-12-28 20:12:42 +0000

    added EfficiencyWindow to generate bar chart of manual vs machine results

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Knapp / FQV Reader
 
-Knapp / FQV Reader is a python application that will open pre-generated FQV or Knapp machine run files.
+Knapp / FQV Reader is a python application that will open pre-generated FQV or Knapp machine run files. A comparison of the machine vs manual inspection can be viewed and a bar chart with the machine vs manual efficiency can be generated.
 
 ## Knapp
 The Knapp theory, introduced by Dr. Julius Z. Knapp over two decades ago, enables an objective comparison and validation of particulate inspection techniques. This theory is grounded in the understanding that particulate inspection, whether performed by humans or automated systems, is inherently probabilistic rather than deterministic. This probabilistic nature is attributed to factors such as the size, typology, and contrast of encountered particulates.
@@ -20,18 +20,52 @@ The FQV process involves the following steps:
 6. After 10 inspections, record the FQV for each container
 7. Calculate the number of times each container has been rejected by the five operators
 8. Calculate the FQV for each container using the formula:
+
     FQV (container number. xxx) = (n/N) * 10
+
     n: Number of times the container has been rejected
+
     N: Total number of inspections
+
     Example:
     FQV (container number. 51) = (41/50) * 10 = 8.2
 
 The Knapp method emphasizes containers 7 to 10 for efficiency calculations. Containers with FQV categories 0 to 3 are considered good, while categories 4 to 6 constitute a grey zone, indicating instability. Ideally, the grey zone should be minimal or nonexistent.
 
-## FQA
-The FQA (Factor quality automatic) is defined by an automatic inspection system
+## FQA (Factor Quality Automatic)
+The FQA is defined by an automatic inspection system:
+
 1. Load the Knapp batch (250 containers) on the machine. The containers will be loaded in subbatches corresponding of the number of spindles, i.e. with 40 spindles machines the containers will be loaded in order starting from the position 1 to 40, than 41 to 80, and so on. 
+
 2. Run inspection (usually 10 inspections per run).
+
+## FQA vs FQV (Efficiency calculation)
+
+The FQA categories included between 7 and 10 (by automatic inspection) are considered for the final calculation of rejection efficiency.
+
+1. Calculate the sum of the FQA:
+
+FQA(7,10) = sum FQA i
+
+i=1
+
+FQA = quality factor obtained by automatic inspection
+
+i = containers included in the reject area (7 ≤ FQA ≤ 10)
+
+2. Calculate the sum of the FQV:
+
+FQV = Σ FQV i
+
+i=1
+
+FQV = quality factor obtained by traditional visual inspection
+
+i = containers included in the reject area (7 ≤ FQA ≤ 10)
+
+The ratio between the above sum totals determines the efficiency of the visual inspection related to the automatic inspection results: if this ratio is less than 100% the visual inspection values may contain some non-detected particles.
+
+[FQV/FQA (7,10)] x 100
 
 
 ## Installation
--- main.py
+++ main.py
@@ -20,6 +20,8 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import Qt, pyqtSlot
 from PyQt5.QtGui import QBrush, QColor, QPalette
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 
 class MainApp(QWidget):
@@ -413,6 +415,7 @@ class CompareResults(QWidget):
         super().__init__()
         self.manual_results = manual_results or {}
         self.machine_results = machine_results or {}
+        self.efficiency_window = None
         self.CompareResultsUI()
 
     def CompareResultsUI(self):
@@ -445,15 +448,31 @@ class CompareResults(QWidget):
             if int(machine_item.text()) > 7:
                 machine_item.setData(Qt.UserRole, "high_value")
 
+            self.manual_containers[f"container_{container}"] = int(manual_item.text())
+            self.machine_containers[f"container_{container}"] = int(machine_item.text())
+
         colourCell = ColourCell()
         table.setItemDelegate(colourCell)
+        self.show_efficiency_button = QPushButton("Calculate efficiency", self)
+        self.show_efficiency_button.clicked.connect(self.show_efficiency)
         self.close_button = QPushButton("Close", self)
         self.close_button.clicked.connect(self.close)
         button_layout = QVBoxLayout()
+        button_layout.addWidget(self.show_efficiency_button)
         button_layout.addWidget(self.close_button)
         self.compare_results_widget.addWidget(table)
         self.compare_results_widget.addLayout(button_layout)
         self.setLayout(self.compare_results_widget)
+
+    def show_efficiency(self):
+        if self.efficiency_window is not None:
+            self.efficiency_window.showNormal()
+            self.efficiency_window.activateWindow()
+        else:
+            self.efficiency_window = EfficiencyWindow(
+                self.manual_containers, self.machine_containers
+            )
+            self.efficiency_window.show()
 
 
 class ColourCell(QStyledItemDelegate):
@@ -477,6 +496,128 @@ class ColourCell(QStyledItemDelegate):
             palette = option.palette
             palette.setColor(QPalette.Text, Qt.white)
             option.palette = palette
+
+
+class EfficiencyWindow(QMainWindow):
+    def __init__(self, manual_results=None, machine_results=None):
+        super().__init__()
+
+        self.setWindowTitle("Machine vs Manual")
+        self.setGeometry(600, 100, 600, 400)
+
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+        self.manual_containers = manual_results or {}
+        self.machine_containers = machine_results or {}
+
+        self.efficiency_canvas = FigureCanvas(Figure(figsize=(8, 6), dpi=100))
+        self.central_layout = QVBoxLayout(self.central_widget)
+        self.central_layout.addWidget(self.efficiency_canvas)
+
+        self.plot_bar_chart()
+
+    def plot_bar_chart(self):
+        ax = self.efficiency_canvas.figure.add_subplot(111)
+
+        if not self.manual_containers or not self.machine_containers:
+            ax.text(
+                0.5,
+                0.5,
+                "No data to plot",
+                ha="center",
+                va="center",
+                fontsize=12,
+                color="gray",
+            )
+        else:
+            values = list(range(11))
+            manual_counts = [
+                list(self.manual_containers.values()).count(value) for value in values
+            ]
+            machine_counts = [
+                list(self.machine_containers.values()).count(value) for value in values
+            ]
+
+            bar_width = 0.35
+            index = range(len(values))
+
+            for i, count in enumerate(manual_counts):
+                ax.bar(
+                    i, count, bar_width, color="blue", label="Manual" if i == 0 else ""
+                )
+                ax.text(
+                    i,
+                    count,
+                    str(count),
+                    ha="center",
+                    va="bottom",
+                    fontsize=8,
+                    color="black",
+                )
+
+            for i, count in enumerate(machine_counts):
+                ax.bar(
+                    i + bar_width,
+                    count,
+                    bar_width,
+                    color="orange",
+                    label="Machine" if i == 0 else "",
+                )
+                ax.text(
+                    i + bar_width,
+                    count,
+                    str(count),
+                    ha="center",
+                    va="bottom",
+                    fontsize=8,
+                    color="black",
+                )
+
+            ax.set_xlabel("No. rejected by operators and machine")
+            ax.set_ylabel("Times Rejected")
+            ax.set_title("FQV/A graphical distribution")
+            ax.set_xticks([i + bar_width / 2 for i in index])
+            ax.set_xticklabels(values)
+
+            # Calculate efficiency only for values 7 or above
+            total_manual = sum(manual_counts[7:])
+            total_machine = sum(machine_counts[7:])
+            efficacy_manual_to_machine = (
+                total_manual / total_machine * 100 if total_machine > 0 else 0
+            )
+            efficacy_machine_to_manual = (
+                total_machine / total_manual * 100 if total_manual > 0 else 0
+            )
+
+            text_manual_vs_machine = (
+                f"Manual vs Machine Efficiency: {efficacy_manual_to_machine:.2f}%"
+            )
+            text_machine_vs_manual = (
+                f"Machine vs Manual Efficiency: {efficacy_machine_to_manual:.2f}%"
+            )
+
+            ax.annotate(
+                text_manual_vs_machine,
+                xy=(0.5, 0.95),
+                xycoords="axes fraction",
+                ha="center",
+                va="center",
+                fontsize=10,
+                color="blue",
+            )
+            ax.annotate(
+                text_machine_vs_manual,
+                xy=(0.5, 0.90),
+                xycoords="axes fraction",
+                ha="center",
+                va="center",
+                fontsize=10,
+                color="orange",
+            )
+
+            ax.legend()
+
+        self.efficiency_canvas.draw()
 
 
 if __name__ == "__main__":
