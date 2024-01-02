2ce116c7898bac6423185211e1072c897da1bc64
Kevin Cornish <kevinz_c@live.com>
2024-01-02 01:02:18 +0000

    add small dialog box to enter number of containers on FQV creation

--- widgets/create_fqv.py
+++ widgets/create_fqv.py
@@ -13,8 +13,31 @@ from PyQt6.QtWidgets import (
     QFileDialog,
     QTableWidget,
     QTableWidgetItem,
+    QDialog,
+    QDialogButtonBox,
+    QSpinBox,
 )
 from constants import CONTAINER_START, CONTAINER_END
+
+
+class ContainerInput(QDialog):
+    def __init__(self, parent=None):
+        super(ContainerInput, self).__init__(parent)
+        self.setWindowTitle("Number of Containers")
+        self.layout = QVBoxLayout(self)
+
+        self.label = QLabel("Enter the number of containers:")
+        self.container_input = QSpinBox(self)
+
+        self.buttons = QDialogButtonBox(
+            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+        )
+        self.buttons.accepted.connect(self.accept)
+        self.buttons.rejected.connect(self.reject)
+
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.container_input)
+        self.layout.addWidget(self.buttons)
 
 
 class CreateFQV(QWidget):
@@ -28,6 +51,7 @@ class CreateFQV(QWidget):
         self.setWindowTitle("Create FQV Results")
         self.manual_results = {}
         self.fqv_containers = {}
+        self.num_containers = 0
         self.CreateFQVUI()
 
     def CreateFQVUI(self):
@@ -35,6 +59,8 @@ class CreateFQV(QWidget):
         Setup the UI for creating FQV results.
         """
         self.fqv_widget = QVBoxLayout()
+
+        self.show_container_input()
 
         self.setup_table()
 
@@ -56,17 +82,30 @@ class CreateFQV(QWidget):
 
         self.setLayout(self.fqv_widget)
 
+    def show_container_input(self):
+        container_dialog = ContainerInput()
+        result = container_dialog.exec()
+
+        if result == QDialog.DialogCode.Accepted:
+            self.num_containers = int(container_dialog.container_input.text())
+
     def setup_table(self):
         """
         Setup the table for displaying FQV results.
         """
         self.fqv_results_table = QTableWidget()
-        self.fqv_results_table.setRowCount(CONTAINER_END)
+        self.fqv_results_table.setRowCount(self.num_containers)
         self.fqv_results_table.setColumnCount(1)
         self.fqv_results_table.setHorizontalHeaderLabels(["Manual"])
 
-        for container in range(CONTAINER_START, CONTAINER_END + 1):
+        for container in range(1, self.num_containers + 1):
             self.fqv_containers[container] = QTableWidgetItem("0")
+            self.fqv_results_table.setItem(
+                container - 1, 0, self.fqv_containers[container]
+            )
+
+        for container in range(self.num_containers + 1, CONTAINER_END + 1):
+            self.fqv_containers[container] = QTableWidgetItem("-1")
             self.fqv_results_table.setItem(
                 container - 1, 0, self.fqv_containers[container]
             )
@@ -108,15 +147,35 @@ class CreateFQV(QWidget):
                 pre_made_results = read_pickle_file(fileName)
                 for container in range(CONTAINER_START, CONTAINER_END + 1):
                     result = pre_made_results.get(f"container_{container}", 0)
-                    self.fqv_containers[container].setText(str(result))
+                    if container <= self.num_containers:
+                        self.fqv_containers[container].setText(str(result))
+                    else:
+                        self.num_containers += 1
+                        self.fqv_results_table.setRowCount(self.num_containers)
+                        self.fqv_containers[container] = QTableWidgetItem(str(result))
+                        self.fqv_results_table.setItem(
+                            container - 1, 0, self.fqv_containers[container]
+                        )
             elif fileName.endswith(".xml"):
                 try:
                     root = ET.parse(fileName).getroot()
                     container_number = 0
                     for type_tag in root.findall("Sample/Manual"):
                         container_number += 1
-                        self.fqv_containers[container_number].setText(
-                            str(type_tag.text)
-                        )
+                        if container_number <= self.num_containers:
+                            self.fqv_containers[container_number].setText(
+                                str(type_tag.text)
+                            )
+                        else:
+                            self.num_containers += 1
+                            self.fqv_results_table.setRowCount(self.num_containers)
+                            self.fqv_containers[container_number] = QTableWidgetItem(
+                                str(type_tag.text)
+                            )
+                            self.fqv_results_table.setItem(
+                                container_number - 1,
+                                0,
+                                self.fqv_containers[container_number],
+                            )
                 except KeyError:
                     pass
